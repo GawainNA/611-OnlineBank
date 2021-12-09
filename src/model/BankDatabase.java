1c93d9b282328a345dc79fa5fe5b6a8bb18f9a11
Yan Tong <ty760206236@gmail.com>
2021-12-09 14:13:07 -0500

    add BankDatabase interface

--- src/model/BankDatabase.java
+++ src/model/BankDatabase.java
@@ -1,11 +1,40 @@
 package model;
 
+import model.account.Account;
+import model.user.Customer;
+
+// TODO: add add***() methods   C
+// TODO: add delete***() methods    D
+// TODO: add update***() methods    U
+// TODO: add flush() methods
+// TODO: add read() methods
+
 /**
- * This class stores all objects that needed to be persisted. 
- * Also it can search object through id.
- * Just use this class as a database interface.
- * Singleton.
+ * BankDatabase interface
+ * Should support functions to CRUD
  */
-public class BankDatabase {
-    // TODO:
+public interface BankDatabase {
+    /**
+     * 
+     * @param id: customer id
+     * @return customer object if id exist. or null if no customer with such id.
+     */
+    public Customer getCustomerById(int id);
+
+
+    /**
+     * 
+     * @param id account id
+     * @return account object if id exist. or null if no account with such id
+     */
+    public Account getAccountById(int id);
+
+
+    /**
+     * 
+     * @param id loan id
+     * @return loan object if id exist. or null if no loan with such id
+     */
+    public Loan getLoanById(int id);
+    
 }
