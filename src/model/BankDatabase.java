298e98781d769c337ea7036e5f01e78d99dc3a13
Yan Tong <ty760206236@gmail.com>
2021-12-09 00:37:05 -0500

    add BankDatabase class

--- src/model/BankDatabase.java
+++ src/model/BankDatabase.java
@@ -1,10 +1,11 @@
 package model;
 
 /**
- * Singleton. This class stores all objects that needed to be persisted. 
+ * This class stores all objects that needed to be persisted. 
  * Also it can search object through id.
  * Just use this class as a database interface.
+ * Singleton.
  */
 public class BankDatabase {
-    
+    // TODO:
 }
