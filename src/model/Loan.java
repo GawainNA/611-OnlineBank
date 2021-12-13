d338bbe7472b8ca26a85a0b18e6c8ee70d4e7d7f
Yan Tong <ty760206236@gmail.com>
2021-12-12 21:52:03 -0500

    add getter and setter

--- src/model/Loan.java
+++ src/model/Loan.java
@@ -19,4 +19,40 @@ public class Loan implements Persistable{
         isReedemed = false;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public Currency getLoanPrice() {
+        return loanPrice;
+    }
+
+    public void setLoanPrice(Currency loanPrice) {
+        this.loanPrice = loanPrice;
+    }
+
+    public Collateral getCollateral() {
+        return collateral;
+    }
+
+    public void setCollateral(Collateral collateral) {
+        this.collateral = collateral;
+    }
+
+    public boolean isReedemed() {
+        return isReedemed;
+    }
+
+    public void setReedemed(boolean isReedemed) {
+        this.isReedemed = isReedemed;
+    }
+
+    public boolean isAgreed() {
+        return isAgreed;
+    }
+
+    public void setAgreed(boolean isAgreed) {
+        this.isAgreed = isAgreed;
+    }
+
 }
