bde07d69b3aba9ec806fd657c8b64b4718899d7c
CHTONGY <ty760206236@gmail.com>
2021-12-14 11:34:21 -0500

    implement deposit and withdraw

--- src/model/account/CheckingAccount.java
+++ src/model/account/CheckingAccount.java
@@ -1,27 +1,44 @@
 package model.account;
 
+import model.Bank;
 import model.ErrCode;
 import model.currency.Currency;
 import model.currency.CurrencyType;
 
 public class CheckingAccount extends Account{
     public void deposit(double amount, CurrencyType currencyType) {
-        // TODO:
+        deposit(new Currency(currencyType, amount));
     }
 
     public void deposit(Currency currency) {
-        // TODO:
+        addCurrency(currency);
+        Bank.getInstance().getBankDatabase().update();
     }
 
     public ErrCode withdraw(double amount, CurrencyType currencyType) {
-        // TODO:
-
-        return null;
+        return withdraw(new Currency(currencyType, amount));
     }
 
     public ErrCode withdraw(Currency currency) {
-        // TODO:
+        ErrCode errCode = new ErrCode(true, "success");
 
-        return null;
+        Currency deposit = this.getCurrencyByType(currency.getCurrencyType());
+        // check whether has specific currency type
+        if(deposit == null) {
+            errCode.isSuccess = false;
+            errCode.errMsg = "do not have specific type of currency";
+            return errCode;
+        }
+        // check whether has enough money
+        if(deposit.getAmount() < currency.getAmount()) {
+            errCode.isSuccess = false;
+            errCode.errMsg = "do not have enough money";
+            return errCode;
+        }
+
+        minusCurrency(currency);  
+        Bank.getInstance().getBankDatabase().update(); 
+
+        return errCode;
     }
 }
--- src/model/account/SavingAccount.java
+++ src/model/account/SavingAccount.java
@@ -1,5 +1,6 @@
 package model.account;
 
+import model.Bank;
 import model.ErrCode;
 import model.currency.Currency;
 import model.currency.CurrencyType;
@@ -8,27 +9,51 @@ public class SavingAccount extends Account {
     private double interest;
 
     public SavingAccount(double interest) {
+        this.setInterest(interest);
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
         this.interest = interest;
     }
 
     public void deposit(double amount, CurrencyType currencyType) {
-        // TODO:
+        deposit(new Currency(currencyType, amount));
     }
 
     public void deposit(Currency currency) {
-        // TODO:
+        addCurrency(currency);
+        Bank.getInstance().getBankDatabase().update();
     }
 
     public ErrCode withdraw(double amount, CurrencyType currencyType) {
-        // TODO:
-
-        return null;
+        return withdraw(new Currency(currencyType, amount));
     }
 
     public ErrCode withdraw(Currency currency) {
-        // TODO:
+        ErrCode errCode = new ErrCode(true, "success");
 
-        return null;
+        Currency deposit = this.getCurrencyByType(currency.getCurrencyType());
+        // check whether has specific currency type
+        if(deposit == null) {
+            errCode.isSuccess = false;
+            errCode.errMsg = "do not have specific type of currency";
+            return errCode;
+        }
+        // check whether has enough money
+        if(deposit.getAmount() < currency.getAmount()) {
+            errCode.isSuccess = false;
+            errCode.errMsg = "do not have enough money";
+            return errCode;
+        }
+
+        minusCurrency(currency);  
+        Bank.getInstance().getBankDatabase().update(); 
+
+        return errCode;
     }
     
 }
