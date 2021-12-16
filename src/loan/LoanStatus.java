2a1d6de9a44b778fe21c960fac2c6241c84f4dac
CHTONGY <ty760206236@gmail.com>
2021-12-15 19:41:35 -0500

    add LoanStatus

---
+++ src/loan/LoanStatus.java
@@ -0,0 +1,38 @@
+package loan;
+
+public enum LoanStatus {
+    LOAN_PENDING("pending"),    // submit, wait for manager to deal
+    LOAN_AGREED("agreed"),  // manager pass this loan
+    LOAN_REFUSED("refused"),    // manager refuse this loan
+    LOAN_REDEEMED("redeemed"),  // customer redeem this loan
+    UNDEFINED("undefined"); // undefine status, use to handle invalid input
+    
+    private String status;
+
+    private LoanStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStr() {
+        return this.status;
+    }
+
+    /**
+     * 
+     * @param status now supporting: pending, agreed, refused, redeemed
+     * @return enum of loan status
+     */
+    public static LoanStatus str2LoanStatus(String status) {
+        if(LOAN_PENDING.getStr().equalsIgnoreCase(status)) {
+            return LOAN_PENDING;
+        } else if(LOAN_AGREED.getStr().equalsIgnoreCase(status)) {
+            return LOAN_AGREED;
+        } else if(LOAN_REFUSED.getStr().equalsIgnoreCase(status)) {
+            return LOAN_REFUSED;
+        } else if(LOAN_REDEEMED.getStr().equalsIgnoreCase(status)) {
+            return LOAN_REDEEMED;
+        }
+
+        return UNDEFINED;
+    }
+}
